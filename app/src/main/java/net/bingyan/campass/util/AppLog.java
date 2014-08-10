d825f6213ae81cd139cbbb2b5bd175986033b78c
张鹏宇 <zhangpengyu@zhangpengyudeMacBook-Air-5.local>
2014-08-10 21:12:21 +0800

    modify AppLog

--- app/src/main/java/net/bingyan/campass/util/AppLog.java
+++ app/src/main/java/net/bingyan/campass/util/AppLog.java
@@ -1,46 +1,120 @@
 package net.bingyan.campass.util;
 
+import android.text.TextUtils;
 import android.util.Log;
+
+import net.bingyan.campass.BuildConfig;
 
 /**
  * Created by ant on 14-8-9.
  */
 public class AppLog {
-    String TAG = "hustpass";
-    String className;
-    public AppLog(Class<?> clazz) {
-        className = '[' + clazz.getSimpleName() + ']';
+    //调试模式输出log，发布版本不输出
+    public static boolean debug = BuildConfig.DEBUG;
+    private static String mTag;
+
+    //log长度有限制
+    public static final int MaxLength = 4000;
+
+    public static final int Debug = 1;
+    public static final int Info = 2;
+    public static final int Warn = 3;
+    public static final int Error = 4;
+
+    public <T> AppLog(Class<T> class1) {
+        mTag = class1.getCanonicalName();
     }
 
-    public void v(String msg) {
-        if (msg != null) {
-            Log.v(TAG, className + msg);
-        } else {
-            Log.v(TAG, msg);
+    public static void d(String msg) {
+        if (!debug || TextUtils.isEmpty(msg))
+            return;
+        logMessage(msg, Debug);
+    }
+
+    public static void i(String msg) {
+        if (!debug || TextUtils.isEmpty(msg))
+            return;
+        logMessage(msg, Info);
+    }
+
+    public static void w(String msg) {
+        if (!debug || TextUtils.isEmpty(msg))
+            return;
+        logMessage(msg, Warn);
+    }
+
+    public static void e(String msg) {
+        if (!debug || TextUtils.isEmpty(msg))
+            return;
+        logMessage(msg, Error);
+    }
+
+    public static void e(Throwable t) {
+        if (!debug || TextUtils.isEmpty(t.getMessage())) {
+            return;
+        }
+        Log.e(mTag, getLogMsg(t.getMessage()));
+    }
+
+    public static void e(String msg, Throwable t) {
+        if (!debug || TextUtils.isEmpty(msg)) {
+            return;
+        }
+        Log.e(mTag, getLogMsg(msg), t);
+    }
+
+    private static void logMessage(String msg, int method) {
+        for (int i = 0; i <= msg.length() / MaxLength; i++) {
+            int start = i * MaxLength;
+            int end = (i + 1) * MaxLength;
+            end = end > msg.length() ? msg.length() : end;
+            switch (method) {
+                case Warn:
+                    Log.w(mTag, msg.substring(start, end));
+                    break;
+                case Debug:
+                    Log.d(mTag, msg.substring(start, end));
+                    break;
+                case Info:
+                    Log.i(mTag, msg.substring(start, end));
+                    break;
+                case Error:
+                    Log.e(mTag, msg.substring(start, end));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    public void d(String msg) {
-        if (msg != null) {
-            Log.d(TAG, className + msg);
-        } else {
-            Log.d(TAG, msg);
+    private static String getFunctionName() {
+        StackTraceElement[] sts = Thread.currentThread().getStackTrace();
+        if (sts == null) {
+            return null;
         }
+
+        for (StackTraceElement st : sts) {
+            if (st.isNativeMethod()) {
+                continue;
+            }
+            if (st.getClassName().equals(Thread.class.getName())) {
+                continue;
+            }
+            //排除掉AppLog输出
+            if (st.getClassName().equals(AppLog.class.getName())) {
+                continue;
+            }
+
+            return "[" + Thread.currentThread().getName() + "(" + Thread.currentThread().getId()
+                    + "): " + st.getFileName() + ":" + st.getMethodName() + ":" + st.getLineNumber() + "]";
+        }
+
+        return null;
     }
 
-    public void i(String msg) {
-        if (msg != null) {
-            Log.i(TAG, className + msg);
-        } else {
-            Log.i(TAG, msg);
-        }
-    }
-
-    public void e(String msg) {
-        if (msg != null) {
-            Log.e(TAG, className + msg);
-        } else {
-            Log.e(TAG, msg);
-        }
+    private static String getLogMsg(String msg) {
+        String functionName = getFunctionName();
+        String message = (functionName == null ? msg : (functionName + " - " + msg));
+        return message;
     }
 }
